dbe2aef3abd72b5ae619122d276de8580efbd6c1
RogelioMarfil <77865994+RogelioMarfil@users.noreply.github.com>
2026-03-29 17:05:20 +0800

    for update

--- src/Components/NavBar/Programs/Programs.jsx
+++ src/Components/NavBar/Programs/Programs.jsx
@@ -6,6 +6,12 @@ import Pic_3 from '../../../assets/5.png'
 import Icon_1 from '../../../assets/endoscope.png'
 import Icon_2 from '../../../assets/ultrasound.png'
 import Icon_3 from '../../../assets/Radiology.png'
+import Pic_6 from '../../../assets/6.png'
+import Pic_7 from '../../../assets/7.png'
+import Pic_8 from '../../../assets/8.png'
+import Icon_10 from '../../../assets/10.png'
+import Icon_11 from '../../../assets/11.png'
+import Icon_12 from '../../../assets/12.png'
 
 const Programs = () => {
   return (
@@ -31,7 +37,29 @@ const Programs = () => {
             <p>Radiology</p>
           </div>
         </div>
+        <div className='program'>
+           <img src={Pic_6} alt=""/>
+            <div className="caption">
+              <img src={Icon_10} alt="" />
+               <p>Emergency Room</p>
+          </div>
+        </div>
+        <div className='program'>
+          <img src={Pic_7} alt=""/>
+            <div className="caption">
+              <img src={Icon_11} alt="" />
+               <p>Laboratory & Diagnostics</p>
+         </div>
+        </div>
+        <div className='program'>
+          <img src={Pic_8} alt=""/>
+           <div className="caption">
+            <img src={Icon_12} alt="" />
+            <p>Out Patient</p>
+          </div>
+        </div>
     </div>
+    
   )
 }
 
