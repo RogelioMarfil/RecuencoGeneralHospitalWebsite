180433fbac9d512061258e1ab7738576bea5ab4b
RogelioMarfil <77865994+RogelioMarfil@users.noreply.github.com>
2026-04-01 18:04:08 +0800

    updated

--- src/Components/NavBar/Hero/Hero.jsx
+++ src/Components/NavBar/Hero/Hero.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import './Hero.css'
 
-const Hero = ({onScroll}) => {
+const Hero = ({ onScroll }) => {
   return (
-    <div className='hero Container'>
-        <div className="hero-text">
-            <h1>Recuenco General Hospital</h1>
-            <p>Our hospital is dedicated to providing you 
-                with the best possible care in a compassionate 
-                and excellent manner. We go above and 
-                beyond treatment to give you and your 
-                family the solace, confidence, and recovery 
-                you deserve</p>
-                <button className='Btn'onClick={onScroll}>Explore More <img src="" alt="" /> </button>
-        </div>
+    <div className="hero">
+      <div className="hero-text">
+        <h1>Recuenco General Hospital</h1>
+        <p>
+          Our hospital is committed to providing you 
+          with the best care possible by offering efficient,
+          adequate, cost-effective, and high-quality 
+          healthcare services. We go above and 
+          beyond treatment to give you and your 
+          family with the support, confidence, and recovery 
+          you deserve 
+        </p>
+        <p>"Sa Recuenco, Alaga ka!"</p>
+        <button className="Btn" onClick={onScroll}>
+          Explore More <img src="" alt="" />
+        </button>
+      </div>
     </div>
   )
 }
