0a5bcde601c4e6e83d1ba543822cfbf740f85f55
RogelioMarfil <77865994+RogelioMarfil@users.noreply.github.com>
2026-04-10 20:57:22 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,41 @@
-import React, { useEffect } from 'react'
-import NavBar from './Components/NavBar/NavBar'
-import Hero from './Components/NavBar/Hero/Hero'
-import Programs from './Components/NavBar/Programs/Programs'
-import Title from './Components/Title/Title'
-import Services from './Components/Services/Services'
-import hospitalLogo from "../src/assets/Logo.png";
-import Footer from './Components/Footer/Footer'
-import ChatBot from './Components/ChatBot/ChatBot'
-import Complaints from './Components/Complains/Complain'
+import React, { useEffect } from 'react';
+import Hero from './Components/NavBar/Hero/Hero';
+import Programs from './Components/NavBar/Programs/Programs';
+import Title from './Components/Title/Title';
+import hospitalLogo from "../src/assets/Nav_Logo/Logo.png";
+import Footer from './Components/Footer/Footer';
+import Events from './Components/Events/Events';
+import Accreditations from './Components/Accredetation/Accreditations';
+import AdvisoryPopup from './Components/Advisory/AdminAdvisoryManager';
 
 function App () {
   useEffect(() => {
-     document.title = "Recuenco General Hospital";
+    document.title = "Recuenco General Hospital";
     const favicon = document.querySelector("link[rel='icon']") || document.createElement("link");
     favicon.rel = "icon";
     favicon.type = "image/png";
     favicon.href = hospitalLogo;
     document.head.appendChild(favicon);
   }, []);
+
   const scrollToAbout = () => {
-   document.getElementById("about").scrollIntoView({ behavior: "smooth" });
+    document.getElementById("about").scrollIntoView({ behavior: "smooth" });
   };
+
   return (
     <div>
-      <NavBar/>
+      <AdvisoryPopup />
+
       <Hero onScroll={scrollToAbout}/>
-      <div className="container"></div>
+      <div className="container">
         <Title title='OUR SERVICES'/>
         <Programs/>
-        <Services/>
-        <ChatBot/>
-         <Complaints/>
+        <Events/>
+        <Accreditations/>
         <Footer/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/Components/NavBar/Programs/Programs.jsx
+++ src/Components/NavBar/Programs/Programs.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import './Programs.css'
-import Pic_1 from '../../../assets/3.png'
-import Pic_2 from '../../../assets/4.png'
-import Pic_3 from '../../../assets/5.png'
-import Icon_1 from '../../../assets/endoscope.png'
-import Icon_2 from '../../../assets/ultrasound.png'
-import Icon_3 from '../../../assets/Radiology.png'
+import Pic_1 from '../../../assets/Services/3.png'
+import Pic_2 from '../../../assets/Services/4.png'
+import Pic_3 from '../../../assets/Services/5.png'
+import Icon_1 from '../../../assets/Services_Icon/endoscope.png'
+import Icon_2 from '../../../assets/Services_Icon/ultrasound.png'
+import Icon_3 from '../../../assets/Services_Icon/Radiology.png'
+import Pic_6 from '../../../assets/Services/6.png'
+import Pic_7 from '../../../assets/Services/7.png'
+import Pic_8 from '../../../assets/Services/8.png'
+import Icon_10 from '../../../assets/Services_Icon/10.png'
+import Icon_11 from '../../../assets/Services_Icon/11.png'
+import Icon_12 from '../../../assets/Services_Icon/12.png'
 
 const Programs = () => {
   return (
@@ -31,7 +37,29 @@ const Programs = () => {
             <p>Radiology</p>
           </div>
         </div>
+        <div className='program'>
+           <img src={Pic_6} alt=""/>
+            <div className="caption">
+              <img src={Icon_10} alt="" />
+               <p>Emergency Room</p>
+          </div>
+        </div>
+        <div className='program'>
+          <img src={Pic_7} alt=""/>
+            <div className="caption">
+              <img src={Icon_11} alt="" />
+               <p>Laboratory & Diagnostics</p>
+         </div>
+        </div>
+        <div className='program'>
+          <img src={Pic_8} alt=""/>
+           <div className="caption">
+            <img src={Icon_12} alt="" />
+            <p>Out Patient</p>
+          </div>
+        </div>
     </div>
+    
   )
 }
 
