62240f6f773f807c09565a35a21fccb21e95547b
RogelioMarfil <77865994+RogelioMarfil@users.noreply.github.com>
2026-04-10 15:47:34 +0800

    update

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,13 +1,75 @@
-import React from 'react'
+import React from "react";
 import "./Footer.css";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebookF, faInstagram, faXTwitter } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>© Recuenco General Hospital | All Rights Reserved</p>
+      <div className="footer-content">
+          <div className="footer-column">
+            <h3>Recuenco General Hospital Inc.</h3>
+            <p className="address-link">
+              <a 
+                href="https://www.google.com/maps/search/?api=1&query=No.+68+Sampaloc+Extension,+North+Signal+Village,+Taguig+City,+Metro+Manila,+1630" 
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <span className="line">No. 68 Sampaloc Extension, North Signal Village,Taguig City, Metro Manila, 1630 Philippines</span>
+                
+              </a>
+            </p>
+          </div>
+          <div className="footer-column contact-center">
+            <div className="contact-inner">
+              <div className="contact-row">
+                <span className="contact-label">Email:</span>
+                <a 
+                  href="https://mail.google.com/mail/?view=cm&to=recuencomedical@gmail.com" 
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                >
+                  recuencomedical@gmail.com
+                </a>
+              </div>
+              <div className="contact-row">
+              <span className="contact-label">Phone:</span>
+              <div className="contact-values phone-numbers">
+                <a href="tel:0288085950">(02) 8808-5950</a>
+                <a href="tel:0288379769">(02) 8837-9769</a>
+              </div>
+            </div>
+            </div>
+          </div>
+        <div className="footer-column footer-socials">
+          <div className="social-inner">
+            <div className="social-row">
+              <FontAwesomeIcon icon={faFacebookF} className="social-icon"/>
+              <a href="https://www.facebook.com/recuencogenhospital" target="_blank" rel="noopener noreferrer">
+                Recuenco General Hospital Inc.
+              </a>
+            </div>
+            <div className="social-row">
+              <FontAwesomeIcon icon={faInstagram} className="social-icon"/>
+              <a href="https://www.instagram.com/rghi.2025" target="_blank" rel="noopener noreferrer">
+                Recuenco General Hospital Inc.
+              </a>
+            </div>
+            <div className="social-row">
+              <FontAwesomeIcon icon={faXTwitter} className="social-icon"/>
+              <a href="https://x.com/RGHI2025" target="_blank" rel="noopener noreferrer">
+                Recuenco General Hospital Inc.
+              </a>
+            </div>
+          </div>
+        </div>
+        </div>
+
+      <div className="footer-bottom">
+        © Recuenco General Hospital | All Rights Reserved
+      </div>
     </footer>
   );
-}
+};
 
-export default Footer
+export default Footer;
