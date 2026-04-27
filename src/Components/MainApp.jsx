2235f87076cfec97921060d7d76a1463754ad0d3
RogelioMarfil <77865994+RogelioMarfil@users.noreply.github.com>
2026-04-27 21:42:36 +0800

    update

---
+++ src/Components/MainApp.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./NavBar/NavBar";
+import Home from "../App";
+import NewsUpdate from "../Pages/NewsUpdate";
+import HMO from "../Pages/HMO";
+import Doctors from "../Pages/Doctors";
+import Careers from "../Pages/Careers";
+import About from "../Pages/About";
+import Contact from "../Pages/Contact";
+import "./MainApp.css";
+
+function MainApp() {
+  return (
+    <>
+      <NavBar />
+
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/newsupdate" element={<NewsUpdate />} />
+          <Route path="/hmo" element={<HMO />} />
+          <Route path="/doctors" element={<Doctors />} />
+          <Route path="/careers" element={<Careers />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+    </>
+  );
+}
+
+export default MainApp;
